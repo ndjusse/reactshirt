becd271a119612f6435f62cda5e1b19c2afa798e
unknown <nicaisendjusse@gmail.com>
2024-10-13 06:00:21 +0100

    encore les lien

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,9 +10,7 @@ const Navbar = () => {
                 <NavLink className="navbar-brand" to="/">
                     <img src={logonnn} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
                 </NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+               
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto mb-2 mb-lg-0 fs-5">
                         <li className="nav-item">
